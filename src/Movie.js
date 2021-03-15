480b4ec74589bf46e9eae441dae9a3fe305104ab
Gnong <jinhongdev@gmail.com>
2021-03-15 20:32:20 +0900

    Adding Genres

--- src/Movie.js
+++ src/Movie.js
@@ -2,14 +2,19 @@ import React from "react";
 import PropTypes from 'prop-types';
 import "./Movie.css";
 
-function Movie({ year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
     return (
-        <div class="movie">
-            <img src={poster} alt={title} title={title}/>
-            <div class="movies_movie">
-                <h3 class="movie_title">{title}</h3>
-                <h5 class="movie_year">{year}</h5>
-                <p class="movie_summary">{summary}</p>
+        <div className="movie">
+            <img src={poster} alt={title} title={title} />
+            <div className="movies_movie">
+                <h3 className="movie_title">{title}</h3>
+                <h5 className="movie_year">{year}</h5>
+                <ul className="genres">
+                    {genres.map((genre, idx) =>
+                        <li key={idx} className="genres_genre">{genre}</li>
+                    )}
+                </ul>
+                <p className="movie_summary">{summary}</p>
             </div>
         </div>
     );
@@ -20,7 +25,8 @@ Movie.protoTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default Movie;
